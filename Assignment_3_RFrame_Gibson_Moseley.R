44d91ca29b9d82a4e71b77efc74bb2b582c152d9
Gibby <gibsonmoseley@gmail.com>
2025-03-05 11:32:42 -0800

    Added Combined Histogram for data analysis

--- Assignment_3_RFrame_Gibson_Moseley.R
+++ Assignment_3_RFrame_Gibson_Moseley.R
@@ -16,15 +16,15 @@ data$Date <- as.Date(paste("01", data$Date), format="%d %b %Y")
 #create a year-month column for easier grouping
 data$Date <- format(data$Date, "%Y-%m")
 
+#ensure the columns  and Air.Temperature.Average..degF. &  Snow.Water.Equivalent..in..Start.of.Month.Values are numeric
+data$Air.Temperature.Average..degF. <- as.numeric(data$Air.Temperature.Average..degF.)
+data$Snow.Water.Equivalent..in..Start.of.Month.Values <- as.numeric(data$Snow.Water.Equivalent..in..Start.of.Month.Values)
+
 #print structure
 str(data)
 
 #get summary of data
 summary(data)
-
-#ensure the columns  and Air.Temperature.Average..degF. &  Snow.Water.Equivalent..in..Start.of.Month.Values are numeric
-data$Air.Temperature.Average..degF. <- as.numeric(data$Air.Temperature.Average..degF.)
-data$Snow.Water.Equivalent..in..Start.of.Month.Values <- as.numeric(data$Snow.Water.Equivalent..in..Start.of.Month.Values)
 
 #create a scatter plot with best fit line
 ggplot(data, aes(x = Air.Temperature.Average..degF., y = Snow.Water.Equivalent..in..Start.of.Month.Values)) +
@@ -36,6 +36,19 @@ ggplot(data, aes(x = Air.Temperature.Average..degF., y = Snow.Water.Equivalent..
     		y = "Snow Water Equivalent (in)"
   	)+
   	theme_minimal()
+
+#combined histogram for Air Temperature and Snow Water Equivalent
+ggplot(data) + 
+  geom_histogram(aes(x = Air.Temperature.Average..degF.), bins = 30, fill = "skyblue", color = "black", alpha = 0.7) + 
+  facet_wrap(~ "Temperature") +
+  geom_histogram(aes(x = Snow.Water.Equivalent..in..Start.of.Month.Values), bins = 30, fill = "lightgreen", color = "black", alpha = 0.7) + 
+  facet_wrap(~ "Snow Water Equivalent") +
+  labs(
+      title = "Histogram Comparison of Air Temperature and Snow Water Equivalent",
+      x = "Value",
+      y = "Frequency") +
+  theme_minimal()
+
 
 
 #save the data frame as csv
